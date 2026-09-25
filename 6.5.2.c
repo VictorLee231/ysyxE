9db1f97a7dc35164647626bbefe16ff858ed0ebc
Your Name <t84062840628fp@163.com>
2026-09-25 11:37:48 +0800

    9.25

--- 6.5.2.c
+++ 6.5.2.c
@@ -5,15 +5,15 @@ void diamond(int n, char c)
 
     for (i = 1; i <= n; i++) {
         if (i <= n / 2 + 1)
-            stars = 2 * i - 1;             /* 上半部分：越来越宽 */
+            stars = 2 * i - 1;             
         else
-            stars = 2 * (n - i + 1) - 1;   /* 下半部分：越来越窄 */
+            stars = 2 * (n - i + 1) - 1;   
 
-        spaces = (n - stars) / 2;          /* 左边空几"格" */
+        spaces = (n - stars) / 2;          
 
-        for (j = 1; j <= n; j++) {         /* j 扫整行 1..n，与 spaces 同一套坐标 */
+        for (j = 1; j <= n; j++) {         
             if (j <= spaces || j > spaces + stars)
-                printf("  ");              /* 空格也占两列，和 "%c " 对齐 */
+                printf("  ");              
             else
                 printf("%c ", c);
         }
